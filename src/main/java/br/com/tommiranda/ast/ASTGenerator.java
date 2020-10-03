c11dfcddbad83d1b56297b3ed88130c2345a5de2
Thiago Miranda <marine.rlivielor@gmail.com>
2020-10-03 10:46:25 +0000

    parser final com \n

--- src/main/java/br/com/tommiranda/ast/ASTGenerator.java
+++ src/main/java/br/com/tommiranda/ast/ASTGenerator.java
@@ -136,6 +136,7 @@ public class ASTGenerator {
     }
 
     public boolean ignoreSpaces() {
+        continueIfEOS();
         char c = code.charAt(countExp);
 
         if (c != ' ' && c != '\t' && c != '\n') {
